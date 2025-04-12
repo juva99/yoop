e8059c96a098ba76808591a625f449f05c497703
idonice <idonice3@gmail.com>
2025-04-12 18:46:27 +0300

    update condition for nav and css

--- frontend/app/app/layout.tsx
+++ frontend/app/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Truculenta } from "next/font/google";
 import Navbar from "@/components/navbar";
 import "./globals.css";
-
+const isLoggedIn = true;
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -29,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <Navbar />
+        {isLoggedIn && <Navbar />}
       </body>
     </html>
   );
--- frontend/app/components/navbar.tsx
+++ frontend/app/components/navbar.tsx
@@ -7,17 +7,12 @@ import { AiFillHome } from "react-icons/ai";
 
 const Navbar = () => {
     return (
-        <div className="fixed bottom-0 w-full z-50">
-        <div className='relative h-20 bg-gray-100 flex items-center justify-between px-4'>
-                <div className='absolute -top-7 left-15 border-7 border-gray-100 bg-gradient-to-b from-blue-600 to-blue-800 p-5 rounded-2xl'><Link href={'/create'}>
-                <IoAddCircleOutline size={50} color='white'/></Link></div>
-                <div className='flex flex-1 justify-evenly ml-30'>
+        <nav className="fixed flex bg-gray-100 h-15 items-center justify-around bottom-0 w-full z-50">
+                <Link href={'/'}><IoAddCircleOutline size={40}/></Link>
                 <Link href={'/'}><AiFillHome size={40} /></Link>
                 <Link href={'/games'}><PiSoccerBallFill size={40} color='gray'/></Link>
                 <Link href={'/search'}><IoSearch size={40} color='gray'/></Link>
-                </div>
-            </div>
-        </div>
+        </nav>
     )
 }
 
