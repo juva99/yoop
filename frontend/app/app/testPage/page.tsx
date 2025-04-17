6f666bec603d6b2ec3a36e4d50aa7dbe2c2c92d5
yuval ovadia <yuvalova12@gmai.com>
2025-04-18 00:22:49 +0300

    test case

---
+++ frontend/app/app/testPage/page.tsx
@@ -0,0 +1,15 @@
+const   TestPage=()=>{
+  return(   <div    className="mx-auto w-95/100">
+  <h1    className="text-4xl font-bold text-blue-500"> וואי וואי!</h1>
+  <h2 className="text-1xl p-1 font-bold text-blue-900">     {" "}אוטוטו אתה עולה למגרש, פוגש חברים חדשים, ונותן גול מהסרטים ⚽️🔥{" "}
+  </h2>
+  <p className="p-1 text-gray-800">
+  <strong className="font-bold text-gray-800">
+  {" "}  כל מה שנשאר זה למלא את הפרטים -{" "}
+  </strong>   ותכף תמצא את עצמך אומר: "איך חייתי בלי זה עד עכשיו?!"
+  </p>
+  <p    className="p-1 font-bold text-gray-800">{" "} יאללה, תן פס - ונתקדם &gt;&gt;</p>
+  </div>
+  )
+}
+export default TestPage;
