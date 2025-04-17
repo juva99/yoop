82f96dee028b9dee9272a665eccfd76a29cd35f9
yuval ovadia <yuvalova12@gmai.com>
2025-04-18 00:47:51 +0300

    test2

--- frontend/app/app/testPage/page.tsx
+++ frontend/app/app/testPage/page.tsx
@@ -3,7 +3,7 @@ const   TestPage=()=>{
   <h1    className="text-4xl font-bold text-blue-500"> וואי וואי!</h1>
   <h2 className="text-1xl p-1 font-bold text-blue-900">     {" "}אוטוטו אתה עולה למגרש, פוגש חברים חדשים, ונותן גול מהסרטים ⚽️🔥{" "}
   </h2>
-  <p      className="p-1 text-gray-800">
+  <p   className="p-1 text-gray-800">
   <strong className="font-bold text-gray-800">
   {" "}  כל מה שנשאר זה למלא את הפרטים -{" "}
   </strong>   ותכף תמצא את עצמך אומר: "איך חייתי בלי זה עד עכשיו?!"
