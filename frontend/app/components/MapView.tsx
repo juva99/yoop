7f4c0ad1d964b70f9bd01638c753aeb381b53a28
juva <yuval22599@gmail.com>
2025-04-18 18:31:11 +0300

    fix: format

--- frontend/app/components/MapView.tsx
+++ frontend/app/components/MapView.tsx
@@ -1,26 +1,29 @@
 "use client";
 
-import React, { useState } from "react"
-import { Map, Marker } from "pigeon-maps"
+import React, { useState } from "react";
+import { Map, Marker } from "pigeon-maps";
 
 export function MapView() {
-    const [clickInfo, setClickInfo] = useState<{ lat: number; lng: number } | null>(null);
+  const [clickInfo, setClickInfo] = useState<{
+    lat: number;
+    lng: number;
+  } | null>(null);
 
-    const handleMarkerClick = ({ anchor }: { anchor: [number, number] }) => {
-        const [lat, lng] = anchor;
-        setClickInfo({ lat, lng });
-        console.log("Marker clicked at coordinates:", lat, lng);
-    };
+  const handleMarkerClick = ({ anchor }: { anchor: [number, number] }) => {
+    const [lat, lng] = anchor;
+    setClickInfo({ lat, lng });
+    console.log("Marker clicked at coordinates:", lat, lng);
+  };
 
-    return (
-        <div className="rounded-md overflow-hidden">
-            <Map defaultCenter={[50.879, 4.6997]} defaultZoom={11} height={200}>
-                <Marker
-                    width={50}
-                    anchor={[50.879, 4.6997]}
-                    onClick={handleMarkerClick}
-                />
-            </Map>
-        </div>
-    )
+  return (
+    <div className="overflow-hidden rounded-md">
+      <Map defaultCenter={[50.879, 4.6997]} defaultZoom={11} height={200}>
+        <Marker
+          width={50}
+          anchor={[50.879, 4.6997]}
+          onClick={handleMarkerClick}
+        />
+      </Map>
+    </div>
+  );
 }
