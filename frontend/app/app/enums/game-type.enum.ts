7fb5dd4ae1a22f503ec14db254110f8b90c09f85
yuval ovadia <yuvalova12@gmai.com>
2025-05-02 15:53:41 +0300

    added types to front

---
+++ frontend/app/app/enums/game-type.enum.ts
@@ -0,0 +1,5 @@
+export enum GameType {
+  FootBall = 'football',
+  BasketBall = 'basketball',
+  Tennis = 'tennis',
+}
--- frontend/app/app/types/Player.ts
+++
@@ -1,5 +0,0 @@
-export type Player = {
-  name: string;
-  image: string;
-  phoneNum: string;
-};
