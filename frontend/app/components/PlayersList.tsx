8d175fa5b0899189193402b26ab136b9865b2f6c
yuval ovadia <yuvalova12@gmai.com>
2025-05-02 16:08:33 +0300

    PlayersList types fix

--- frontend/app/components/PlayersList.tsx
+++ frontend/app/components/PlayersList.tsx
@@ -1,10 +1,10 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Player } from "@/app/types/Player";
+import { User } from "@/app/types/User";
 import React from "react";
 import { FaWhatsapp } from "react-icons/fa";
 
 interface Props {
-  players: Player[];
+  players: User[];
 }
 
 const PlayersList: React.FC<Props> = ({ players }) => {
@@ -19,19 +19,23 @@ const PlayersList: React.FC<Props> = ({ players }) => {
           <div key={index} className="flex w-full items-center justify-between">
             <div className="flex items-center gap-2">
               <Avatar className="h-8 w-8 rounded-full border-2 border-white">
-                <AvatarImage src={player.image} alt={player.name} />
-                <AvatarFallback>{player.name.charAt(0)}</AvatarFallback>
+                <AvatarImage src={player.profilePic} alt={player.firstName} />
+                <AvatarFallback>{player.firstName.charAt(0)}</AvatarFallback>
               </Avatar>
-              <span className="text-sm">{player.name}</span>
+              <span className="text-sm">
+                {player.firstName + " " + player.lastName}
+              </span>
             </div>
-            <a
-              href={`https://wa.me/${formatPhoneNumber(player.phoneNum)}`}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="pl-4"
-            >
-              <FaWhatsapp className="text-[#25D366]" />
-            </a>
+            {player.phoneNum && (
+              <a
+                href={`https://wa.me/${formatPhoneNumber(player.phoneNum)}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="pl-4"
+              >
+                <FaWhatsapp className="text-[#25D366]" />
+              </a>
+            )}
           </div>
         ))}
       </div>
