755829506f270d4bcbb9f16926b87955c5dc651b
yuval ovadia <yuvalova12@gmai.com>
2025-05-04 17:53:26 +0300

    added sign out button

--- frontend/app/app/page.tsx
+++ frontend/app/app/page.tsx
@@ -4,6 +4,7 @@ import MapView from "@/components/MapView";
 import Search from "@/components/searchComponents/Search";
 import SearchGame from "@/components/searchComponents/SearchGame";
 import { authFetch } from "@/lib/authFetch";
+import Link from "next/link";
 
 export default async function Home() {
   const res = await authFetch(
@@ -12,9 +13,15 @@ export default async function Home() {
   const data = await res.json();
   return (
     <div className="mb-10 flex flex-col gap-6 p-4 pb-6">
-      <section>
-        <p className="text-title text-2xl font-bold">היי יובל, </p>
-      </section>
+      <div className="flex flex-row items-center mb-2">
+      <p className="text-title text-2xl font-bold">היי יובל, </p>
+      <Link
+        href={"/api/auth/signout"}
+        className="mr-auto rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-600 transition-colors"
+      >
+        התנתק
+      </Link>
+    </div>
       <div className="rounded-2xl shadow-xl">
         <FutureGames games={data} />
       </div>
