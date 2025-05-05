d84bc4d441b06286f42922528e83a88260b1d900
yuval ovadia <yuvalova12@gmai.com>
2025-05-05 22:38:12 +0300

    page block

--- frontend/app/middleware.ts
+++ frontend/app/middleware.ts
@@ -4,6 +4,7 @@ import { getSession } from "./lib/session";
 export default async function middleware(req: NextRequest) {
   const session = await getSession(); 
   if (!session || !session.user) {
+    console.log("user is not signed in - redirecting to login page")
     return NextResponse.redirect(new URL("/auth/login", req.nextUrl));
   }
 
@@ -11,5 +12,5 @@ export default async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/"]
+  matcher: ["/", "/mygames", "/game/create", "/games/create", "/search"]
 }
