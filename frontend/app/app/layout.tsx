95e75b87ffc0d7d1ea1619b4fe1fc6ef37f9269a
guygo <guygo141@gmail.com>
2025-05-16 17:48:34 +0300

    Ovadia the king change it all

--- frontend/app/app/layout.tsx
+++ frontend/app/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist} from "next/font/google";
 import Navbar from "@/components/navbar";
 import "./globals.css";
 
@@ -8,10 +8,7 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en" dir="rtl">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable}  antialiased`}
       >
         <main className="pb-20">{children}</main>
         <Navbar />
