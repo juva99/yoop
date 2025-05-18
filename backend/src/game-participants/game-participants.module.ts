6d99028dcf0cd4fa0cfb5152c2092cc4aba3476c
Rotemh1 <rotemh123@gmail.com>
2025-05-18 13:20:27 +0300

    fix : unneeded typeorm modules

--- backend/src/game-participants/game-participants.module.ts
+++ backend/src/game-participants/game-participants.module.ts
@@ -3,12 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { GameParticipant } from './game-participants.entity';
 import { GameParticipantsController } from './game-participants.controller';
 import { GameParticipantsService } from './game-participants.service';
-import { User } from 'src/users/users.entity';
 import { Game } from 'src/games/games.entity';
 import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([GameParticipant, Game, User]), UsersModule],
+  imports: [TypeOrmModule.forFeature([GameParticipant, Game]), UsersModule],
   exports: [TypeOrmModule, GameParticipantsService],
   controllers: [GameParticipantsController],
   providers: [GameParticipantsService],
--- backend/src/games/games.module.ts
+++ backend/src/games/games.module.ts
@@ -3,14 +3,13 @@ import { GamesController } from './games.controller';
 import { GamesService } from './games.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Game } from './games.entity';
-import { GameParticipant } from 'src/game-participants/game-participants.entity';
 import { WeatherApiModule } from 'src/weather-api/weather-api.module';
 import { GameParticipantsModule } from 'src/game-participants/game-participants.module';
 import { FieldsModule } from 'src/fields/fields.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Game, GameParticipant]),
+    TypeOrmModule.forFeature([Game]),
     WeatherApiModule,
     GameParticipantsModule,
     FieldsModule
