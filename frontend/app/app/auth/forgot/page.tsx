f48bf96c9ab2a94e0a5f4214f0177c88e9188a65
juva <yuval22599@gmail.com>
2025-05-28 12:39:28 +0300

    feat: new forgot password page

---
+++ frontend/app/app/auth/forgot/page.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import LoginForm from "./forgot-form";
+import Image from "next/image";
+import Link from "next/link";
+
+const ForgotPassword = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center gap-8 px-6 py-10">
+      <Image
+        src="/symbol.png"
+        alt="symbol"
+        width={100}
+        height={100}
+        className="absolute top-0 left-0 z-1"
+      />
+
+      <div className="flex w-full justify-center">
+        <Image
+          src="/logo.png"
+          alt="Logo"
+          width={100}
+          height={100}
+          className="mb-4 rounded-full shadow-2xl"
+        />
+      </div>
+      <div className="text-center">
+        <h2 className="text-2xl font-bold">שכחת את הסיסמה?</h2>
+        <p className="mt-2 text-gray-600">
+          הכנס את כתובת האימייל שלך ונשלח לך קישור לאיפוס הסיסמה
+        </p>
+      </div>
+      <div className="mt-6 w-full">
+        <LoginForm />
+      </div>
+      <div className="text-center text-sm text-gray-600">
+        <div className="mt-4 flex w-full items-center justify-center gap-1">
+          <p>נזכרת בסיסמה?</p>
+          <Link href="/auth/login" className="text-title font-medium underline">
+            חזור להתחברות
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ForgotPassword;
