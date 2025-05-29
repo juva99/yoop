f300ebf3266d215ae8d6b10b6a49a09f3e8ec562
juva <yuval22599@gmail.com>
2025-05-29 11:10:11 +0300

    feat: using auth wrapper in reset-password page

--- frontend/app/app/auth/reset-password/page.tsx
+++ frontend/app/app/auth/reset-password/page.tsx
@@ -1,27 +1,11 @@
 import ResetPasswordForm from "@/app/auth/reset-password/reset-password-form";
 import Image from "next/image";
 import Link from "next/link";
+import AuthWrapper from "../authWrapper";
 
 const ResetPassword = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center gap-8 px-6 py-10">
-      <Image
-        src="/symbol.png"
-        alt="symbol"
-        width={100}
-        height={100}
-        className="absolute top-0 left-0 z-1"
-      />
-
-      <div className="flex w-full justify-center">
-        <Image
-          src="/logo.png"
-          alt="Logo"
-          width={100}
-          height={100}
-          className="mb-4 rounded-full shadow-2xl"
-        />
-      </div>
+    <AuthWrapper>
       <div className="text-center">
         <h2 className="text-2xl font-bold">איפוס סיסמה</h2>
         <p className="mt-2 text-gray-600">הזן את הסיסמה החדשה שלך.</p>
@@ -37,7 +21,7 @@ const ResetPassword = () => {
           </Link>
         </div>
       </div>
-    </div>
+    </AuthWrapper>
   );
 };
 
