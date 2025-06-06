e24e69c564709c46f550e477fd43637894a84877
guygo <guygo141@gmail.com>
2025-06-06 09:19:02 +0300

    Entering the Manager Registration Page
    There is no way to go back

--- frontend/app/app/contact/ContactForm.tsx
+++ frontend/app/app/contact/ContactForm.tsx
@@ -20,6 +20,8 @@ import { formSchema } from "@/lib/schemas/manager_signup_schema";
 import { Textarea } from "@/components/ui/textarea";
 import { toast } from "sonner";
 import AuthWrapper from "@/app/auth/authWrapper";
+import Link from "next/link";
+
 
 type Props = {};
 
@@ -176,6 +178,15 @@ const ContactForm: React.FC<Props> = ({}) => {
           </Button>
         </form>
       </Form>
+      <div className="mt-4 flex w-full items-center justify-center gap-1 text-sm">
+        <p className="text-gray-600">כבר רשום למערכת?</p>
+        <Link
+          href="/auth/login"
+          className="font-medium text-blue-500 underline hover:text-blue-700"
+        >
+          התחבר עכשיו
+        </Link>
+      </div>      
     </AuthWrapper>
   );
 };
