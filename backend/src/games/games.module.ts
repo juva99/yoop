d11a141ef3ef74682049bc21e399fa6345858345
idonice <idonice3@gmail.com>
2025-06-10 14:32:33 +0300

    delete import

--- backend/src/games/games.module.ts
+++ backend/src/games/games.module.ts
@@ -1,4 +1,4 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { GamesController } from './games.controller';
 import { GamesService } from './games.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
