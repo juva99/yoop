deb51f4dc9b604a0bb42fff545c18d21b4ce927f
idonice <idonice3@gmail.com>
2025-06-12 18:15:32 +0300

    props removing

--- frontend/app/app/groups/page.tsx
+++ frontend/app/app/groups/page.tsx
@@ -3,11 +3,7 @@ import { Card } from "@/components/ui/card";
 import React from "react";
 import NewGroupBtn from "./NewGroupBtn";
 
-type Props = {
-  // your props here
-};
-
-const page: React.FC<Props> = ({}) => {
+const page: React.FC = () => {
   const groups = [];
   return (
     <div className="flex min-h-[100vh] flex-col gap-5 bg-[url('/search-friends-background.png')] bg-top bg-no-repeat p-5">
