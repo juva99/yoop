af5ba1e40171a471704609b4987651f8559bba9b
idonice <idonice3@gmail.com>
2025-06-12 17:57:44 +0300

    style

--- frontend/app/components/fieldManager/Fields.tsx
+++ frontend/app/components/fieldManager/Fields.tsx
@@ -11,30 +11,28 @@ type Props = {
 
 const Fields: React.FC<Props> = ({ fields }) => {
   return (
-    <div className="p-4">
+    <div className="w-full p-4">
       <div className="flex w-full flex-col items-center gap-6">
-        <Card className="flex flex-col">
-          <h1 className="text-center">המגרשים שלך</h1>
+        <h1 className="text-center">המגרשים שלך</h1>
 
-          {fields.length === 0 ? (
-            <div>
-              <p>אין מגרשים להצגה</p>
-              <Link href={"/field-manager/field/create"}>
-                <Button variant={"submit"}>הוסף מגרש</Button>
-              </Link>
-            </div>
-          ) : (
-            <div className="scrollbar-none max-h-[600px] overflow-y-scroll">
-              {fields.map((field, idx) => (
-                <FieldCard
-                  key={field.fieldId}
-                  field={field}
-                  border={idx < fields.length - 1}
-                />
-              ))}
-            </div>
-          )}
-        </Card>
+        {fields.length === 0 ? (
+          <div>
+            <p>אין מגרשים להצגה</p>
+            <Link href={"/field-manager/field/create"}>
+              <Button variant={"submit"}>הוסף מגרש</Button>
+            </Link>
+          </div>
+        ) : (
+          <div className="scrollbar-none max-h-[600px] w-full overflow-y-scroll">
+            {fields.map((field, idx) => (
+              <FieldCard
+                key={field.fieldId}
+                field={field}
+                border={idx < fields.length - 1}
+              />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
