b3608c56dfa356d266615188092709a0723895de
idonice <idonice3@gmail.com>
2025-06-12 17:22:13 +0300

    .

--- frontend/app/app/friends/page.tsx
+++ frontend/app/app/friends/page.tsx
@@ -42,33 +42,33 @@ export default async function SearchPage({
     <div className="flex min-h-[100vh] flex-col gap-5 bg-[url('/search-friends-background.png')] bg-top bg-no-repeat p-5">
       <Card variant="friends">
         <FriendList currentUserUid={userId} relations={friendRelations} />
-      </Card>
-      <Card variant="friends">
-        <h1>הוספת חברים</h1>
-        <Form action="/friends">
-          <div className="input-wrapper border-title mt-5 mb-5 flex justify-between border-b-1 py-1">
-            <input
-              type="text"
-              name="query"
-              placeholder="חפש חבר"
-              defaultValue={query}
-              className="border-none bg-white bg-none shadow-none focus:border-none focus:ring-0 focus:outline-none"
-            />
+        <div>
+          <h1>הוספת חברים</h1>
+          <Form action="/friends">
+            <div className="input-wrapper border-title mt-5 mb-5 flex justify-between border-b-1 py-1">
+              <input
+                type="text"
+                name="query"
+                placeholder="הקלד שם"
+                defaultValue={query}
+                className="border-none bg-white bg-none shadow-none focus:border-none focus:ring-0 focus:outline-none"
+              />
 
-            <button type="submit">
-              <PiMagnifyingGlassThin />
-            </button>
+              <button type="submit">
+                <PiMagnifyingGlassThin />
+              </button>
+            </div>
+          </Form>
+
+          <div className="scrollbar-none overflow-y-scroll">
+            {friends.length ? (
+              friends.map((friend: User) => (
+                <Friend key={friend.uid} friend={friend} action="add" />
+              ))
+            ) : (
+              <p>אין תוצאות</p>
+            )}
           </div>
-        </Form>
-
-        <div className="scrollbar-none overflow-y-scroll">
-          {friends.length ? (
-            friends.map((friend: User) => (
-              <Friend key={friend.uid} friend={friend} action="add" />
-            ))
-          ) : (
-            <p>אין תוצאות</p>
-          )}
         </div>
       </Card>
     </div>
