aeb998c5b2d65091205a050c6d8aab69c39e3962
Rotemh1 <rotemh123@gmail.com>
2025-06-24 15:26:23 +0300

    set only admin

--- backend/src/field-fetch-api/field-fetch-api.controller.ts
+++ backend/src/field-fetch-api/field-fetch-api.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get, Param, Post } from '@nestjs/common';
 import { FieldFetchApiService } from './field-fetch-api.service';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { Role } from 'src/enums/role.enum';
 
 @Controller('field-fetch-api')
 export class FieldFetchApiController {
   constructor(private readonly fieldFetchApiService: FieldFetchApiService) {}
 
+  @Roles(Role.ADMIN)
   @Get('')
   async getFields() {
     return await this.fieldFetchApiService.getFields();
